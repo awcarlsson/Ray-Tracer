0dc42d573725e04153fd54bda765e5fcb438c567
awcarlsson <awcarlsson1@gmail.com>
2020-06-05 00:58:44 -0500

    Implemented anti-aliasing

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 #include <fstream>
@@ -23,10 +24,13 @@ int main() {
 	bool write_ppm = true;
 	std::string image_name = "hittable";
 
+	// Sets up the image properties
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 384;
+	const int image_width = 1000;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100; // Number of rays to shoot per pixel
 	
+	// Sets up the output
 	std::ofstream outfile;
 	if (write_ppm) {
 		outfile.open("images/" + image_name + ".ppm");
@@ -35,34 +39,34 @@ int main() {
 	else {
 		std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 	}
-	
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
 
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-
+	// Creates the objects populating the world
 	hittable_list world;
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+	world.add(make_shared<sphere>(point3(0, -0.1, -0.5), 0.1));
 
+	camera cam;
+
+	// Cycles through each pixel of the image
 	for (int j = image_height-1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color(0, 0, 0);
+			// Anti-aliasing step:
+			// Shoots samples_per_pixel rays into the pixel at random and averages the colors
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = double(i + random_double()) / (image_width - 1);
+				auto v = double(j + random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
 
-			color pixel_color = ray_color(r, world);
+				pixel_color += ray_color(r, world);
+			}
 
 			if (write_ppm)
-				write_color(outfile, pixel_color);
+				write_color(outfile, pixel_color, samples_per_pixel);
 			else
-				write_color(std::cout, pixel_color);
+				write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	std::cerr << "\nDone.\n";
